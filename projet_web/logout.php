646f534b8c03e58f4ed610a211642d44542a2f1c
Abir Bekkar <bekkarabir20@gmail.com>
2025-04-12 18:13:06 +0100

    Delete projet_web directory

--- projet_web/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // إزالة جميع متغيرات الجلسة
-session_destroy(); // تدمير الجلسة
-
-// توجيه المستخدم إلى صفحة تسجيل الدخول
-header("Location: login.php");
-exit;
-?>
