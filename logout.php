0e94d7ebfb7876314dc4e8d792996824e9b28ebf
Abir Bekkar <bekkarabir20@gmail.com>
2025-04-26 14:10:48 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // إزالة جميع بيانات الجلسة
-session_destroy(); // إنهاء الجلسة بالكامل
-
-header("Location: ../index.php"); // إعادة التوجيه للصفحة الرئيسية
-exit();
-?>
